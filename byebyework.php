33f7c19bf9d588b8e9ed7aff9b21643bc4d1ddc3
Abdullah Karam <abdallakaramdev@gmail.com>
2022-05-01 16:47:54 +0200

    just

--- byebyework.php
+++ byebyework.php
@@ -6,12 +6,15 @@ $result = mysqli_fetch_object($data);
 
 if (!is_null($result)){
 
+    $message = "$result->commit_message";
+//    var_dump($message);
+//    die;
     exec("git add .");
     sleep(3);
-    exec("git commit -m $result->commit_message");
+    exec("git commit -m just cron jop");
     sleep(3);
-    exec("git push origin $result->origin_name");
+    exec("git push -u origin main");
     sleep(3);
-    $updateCron = mysqli_query($connection,"update cron set status = 'completed' where id = '$result->id'");
+    //$updateCron = mysqli_query($connection,"update cron set status = 'completed' where id = '$result->id'");
 }
 mysqli_close($connection);
