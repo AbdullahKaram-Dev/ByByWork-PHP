467695f5d1f4ff2dbed3c7508002fe605232d742
Abdullah Karam <abdallakaramdev@gmail.com>
2022-05-01 16:34:09 +0200

    test commit

--- byebyework.php
+++ byebyework.php
@@ -1,15 +1,17 @@
 <?php
 $projectName = 'php';
 $connection= mysqli_connect('localhost','root','','cron');
-$data = mysqli_query($connection,"select * from cron where project_path = '$projectName' order by date asc limit 1");
+$data = mysqli_query($connection,"select * from cron where project_path = '$projectName' and status = 'uncompleted'");
 $result = mysqli_fetch_object($data);
-/* new update */
-exec("git add .");
-sleep(3);
-exec("git commit -m $result->commit_message");
-sleep(3);
-exec("git push origin $result->origin_name");
-sleep(3);
 
-$updateCron = mysqli_query($connection,"update cron set status = 'completed' where id = '$result->id'");
+if (!is_null($result)){
+
+    exec("git add .");
+    sleep(3);
+    exec("git commit -m $result->commit_message");
+    sleep(3);
+    exec("git push origin $result->origin_name");
+    sleep(3);
+    $updateCron = mysqli_query($connection,"update cron set status = 'completed' where id = '$result->id'");
+}
 mysqli_close($connection);
--- functions.php
+++ functions.php
@@ -1,6 +1,5 @@
 <?php
 declare(strict_types=1);
-
 /** Splat Operator */
 function sum(int...$numbers):int
 {
