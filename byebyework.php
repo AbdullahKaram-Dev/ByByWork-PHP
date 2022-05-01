1f571705ceecd087820b5245b5a1bfce0211c500
Abdullah Karam <abdallakaramdev@gmail.com>
2022-05-01 15:39:47 +0200

    test

--- byebyework.php
+++ byebyework.php
@@ -1,16 +1,17 @@
 <?php
-
 $projectName = 'php';
 $connection= mysqli_connect('localhost','root','','cron');
 $data = mysqli_query($connection,"select * from cron where project_path = '$projectName' order by date asc limit 1");
 $result = mysqli_fetch_object($data);
-
 mysqli_close($connection);
+
 
 exec("git add .");
 sleep(3);
-exec("git commit -m ".$result->commit_message);
+exec("git commit -m $result->commit_message");
 sleep(3);
-exec("git push origin ".$result->origin_name);
+exec("git push origin $result->origin_name");
 sleep(3);
-echo "successfully pushing";
+return "successfully pushing";
+
+
