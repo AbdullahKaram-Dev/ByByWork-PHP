f5a7892f6f023f64bf80577b6651c3d4a7b7792f
Abdullah Karam <abdallakaramdev@gmail.com>
2022-05-03 21:23:15 +0200

    function

--- functions.php
+++ functions.php
@@ -25,4 +25,31 @@ if(!function_exists('sum_three')){
 
 echo sum_three(10,20,20).PHP_EOL;
 
-/** last update commit two */
+/** anonymous function */
+$arrowFunction = fn(int...$numbers): int => array_sum($numbers) + 2222;
+echo $arrowFunction(1,1,1).PHP_EOL;;
+
+/** function named arguments */
+
+function named_argument(string $first_name,string $last_name):string
+{
+    return $first_name.' '.$last_name.PHP_EOL;
+}
+
+echo named_argument(last_name: 'karam',first_name: 'abdullah');
+echo "<br>";
+
+/** nested callable with splat operator in php */
+
+function callableTest(callable $callable,array...$characters):string
+{
+    return call_user_func($callable,$characters);
+}
+
+$userNameCharacters = ['a','b','d','u','l','l','a','h'];
+
+/** result :  (abdullah) */
+
+echo callableTest(function ($characters){
+    return implode('', $characters[0]);
+    },$userNameCharacters);
