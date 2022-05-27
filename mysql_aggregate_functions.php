5dbb62fd1d3d0080cb1efa188f95e399244a4113
Abdullah Karam <abdallakaramdev@gmail.com>
2022-05-27 21:35:43 +0200

    last update aggregate mysql functions

--- mysql_aggregate_functions.php
+++ mysql_aggregate_functions.php
@@ -16,6 +16,7 @@ $query = "SELECT count(ALL payment_history.payed_amount) as total_transaction FR
 /** start sum aggregate function */
 
 $query = "SELECT sum(payment_history.payed_amount) as total_payed_amount FROM `payment_history`";
+$query = "SELECT status,client_course_row_id,sum(payment_history.payed_amount) as min_payed_amount FROM `payment_history` group by payment_history.client_course_row_id";
 
 /** end sum aggregate function */
 
@@ -26,8 +27,29 @@ $query = "SELECT avg(payment_history.payed_amount) as avg_payed_amount FROM `pay
 /** end sum aggregate function */
 
 
+/** start min aggregate function */
+
+$query = "SELECT min(ALL payment_history.payed_amount) as min_payed_amount FROM `payment_history`";
+$query = "SELECT client_course_row_id,min(payment_history.payed_amount) as min_payed_amount FROM `payment_history` group by payment_history.client_course_row_id
+          HAVING min(payment_history.payed_amount) > 50";
+
+/** end min aggregate function */
+
+/** start max aggregate function */
+
+$query = "SELECT max(DISTINCT payment_history.payed_amount) as max_payed_amount FROM `payment_history`";
+
+/** end max aggregate function */
+
+
+/** start group_concat() aggregate function */
+
+$query = "SELECT client_id, group_concat(CONCAT_WS(' - ', clients.client_firstname, clients.client_lastname) SEPARATOR ';') as client_name FROM `clients` group by clients.client_id";
+
+/** end group_concat() aggregate function */
+
 
 $result = mysqli_query($connection, $query);
-$data = mysqli_fetch_all($result, MYSQLI_ASSOC);
-
+//$data = mysqli_fetch_object($result, 'stdClass');
+$data = mysqli_fetch_all($result,MYSQLI_ASSOC);
 dd($data);
